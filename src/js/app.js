53fd720f20f443277dd824f47f98754f5d1e9e86
bstoltz <ben0001@chaosx.esri.com>
2014-07-29 14:49:26 -0400

    Initial project Commit
    Building off Dojo-Bootstrap-Map. Feature layers added.

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+require([
+  'dojo/topic',
+
+  'app/config',
+  'app/widget/Map',
+  'app/widget/NavBar',
+
+  'dojo/i18n!./js/nls/strings.js',
+
+  'dojo/domReady!'],
+function(
+  topic,
+  config, Map, NavBar,
+  strings
+) {
+  'use strict';
+
+  // start map widget
+  var map = new Map({
+    config: config,
+    strings: strings
+  }, 'mapNode');
+  map.startup();
+  map.loadServices();
+
+  // start nav widget
+  var navBar = new NavBar({
+    config: config,
+    strings: strings
+  }, 'navBarNode');
+  navBar.startup();
+
+  // set up topics
+  topic.subscribe('basemap/set', function(args) {
+    map.setBasemap(args.basemap);
+  });
+
+  // set page title
+  window.document.title = strings.appTitle;
+});
---
+++ src/js/config.js
@@ -0,0 +1,17 @@
+define([],function() {
+	return {
+    map: {
+      options: {
+        basemap: 'streets',
+        center: [-80.8395259, 35.2031535],
+        zoom: 11
+      },
+      // TODO: add basemaps
+      basemaps: {
+      }
+    },
+    about: {
+      moreInfoUrl: 'https://github.com/benstoltz/esriInternFinalProject'
+    }
+  };
+});
